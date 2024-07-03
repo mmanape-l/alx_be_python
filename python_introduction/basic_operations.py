447685fbc96d93344f72bd403bc607009a32c014
Nape <mmanape.letlalo@gmail.com>
2024-07-03 14:30:22 +0000

    Move basic_operations.py into python_introduction directory

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,17 @@
+# basic_operations.py
+# Define variables
+number1 = 10
+number2 = 5
+# Print variables to verify
+print(f"number1: {number1}")
+print(f"number2: {number2}")
+# calculate sum
+sum_result = number1 + number2
+# calculate difference
+difference_result = number1 - number2
+# calculate propert
+product_result = number1 * number2
+#Print results 
+print(f"Sum of number1 and number2: {sum_result}")
+print(f"Difference of number1 and number2: {difference_result}")
+print(f"Product of number1 and number2: {product_result}")
