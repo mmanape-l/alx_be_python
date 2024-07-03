01a05c0ba4c628bba5ebffb61d8ef44f3d222dcf
Nape <mmanape.letlalo@gmail.com>
2024-07-03 17:40:44 +0000

    Principal added

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,15 +1,13 @@
 # simple_interest.py 
 #Define Variables
-principle = 1000 # $1000
+principal = 1000 # $1000
 rate = 0.05 # 5% annual interest rate
 time = 3 # 3 years
 # Print variables to verify
-print(f"Principle: ${principle}")
+print(f"Principal: ${principal}")
 print(f"Rate: {rate * 100}%")
 print(f"Time: {time} years")
 # Calculate the simple interest using the formula I = P * R * T
-interest = principle * rate * time
-# Print the result
+interest = principal * rate * time
+# Print the results
 print(f"The simple interest is: {interest}")
-
-
