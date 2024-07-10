dfe28b79a34c4370803501ab3fde90c536164ce2
Nape <mmanape.letlalo@gmail.com>
2024-07-10 13:05:26 +0000

    Correction

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -7,9 +7,9 @@ def get_number(prompt):
 
 def main():
     # Prompt for user input
-    num1 = get_number("Enter the first number: ")
-    num2 = get_number("Enter the second number: ")
-    operation = input("Choose the operation (+, -, *, /): ")
+    num1 = get_number("Enter the first number:")
+    num2 = get_number("Enter the second number:")
+    operation = input("Choose the operation (+, -, *, /):")
 
     # Perform the calculation using match case
     match operation:
