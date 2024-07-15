b5b9651a0685e042fa27c5d53b88df73523d1359
Nape <mmanape.letlalo@gmail.com>
2024-07-15 16:40:34 +0000

    Corrected script

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -27,19 +27,4 @@ class TestSimpleCalculator(unittest.TestCase):
         """Test the multiplication method."""
         self.assertEqual(self.calc.multiply(2, 3), 6)
         self.assertEqual(self.calc.multiply(-1, 1), -1)
-        self.assertEqual(self.calc.multiply(-1, -1), 1)
-        self.assertEqual(self.calc.multiply(0, 0), 0)
-        self.assertEqual(self.calc.multiply(2.5, 3.5), 8.75)
-
-    def test_division(self):
-        """Test the division method."""
-        self.assertEqual(self.calc.divide(6, 3), 2)
-        self.assertEqual(self.calc.divide(-6, 2), -3)
-        self.assertEqual(self.calc.divide(-6, -2), 3)
-        self.assertEqual(self.calc.divide(0, 1), 0)
-        self.assertEqual(self.calc.divide(3.5, 2.5), 1.4)
-        self.assertEqual(self.calc.divide(1, 3), 1 / 3)
-        self.assertEqual(self.calc.divide(1, 0), None)
-
-if __name__ == '__main__':
-    unittest.main()
+        self.assertEqual
