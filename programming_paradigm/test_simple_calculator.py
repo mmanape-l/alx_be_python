95d37b1f63d0549cf81e1375800bf33092c74006
Nape <mmanape.letlalo@gmail.com>
2024-07-15 18:04:05 +0000

    Files updated

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -1,31 +1,35 @@
 # test_simple_calculator.py
 
 import unittest
-import simple_calculator
+from simple_calculator import SimpleCalculator
 
 class TestSimpleCalculator(unittest.TestCase):
 
     def test_add(self):
-        self.assertEqual(simple_calculator.add(1, 2), 3)
-        self.assertEqual(simple_calculator.add(-1, 1), 0)
-        self.assertEqual(simple_calculator.add(-1, -1), -2)
+        calc = SimpleCalculator()  # Create an instance of SimpleCalculator
+        self.assertEqual(calc.add(1, 2), 3)
+        self.assertEqual(calc.add(-1, 1), 0)
+        self.assertEqual(calc.add(-1, -1), -2)
 
     def test_subtract(self):
-        self.assertEqual(simple_calculator.subtract(2, 1), 1)
-        self.assertEqual(simple_calculator.subtract(-1, 1), -2)
-        self.assertEqual(simple_calculator.subtract(-1, -1), 0)
+        calc = SimpleCalculator()  # Create an instance of SimpleCalculator
+        self.assertEqual(calc.subtract(2, 1), 1)
+        self.assertEqual(calc.subtract(-1, 1), -2)
+        self.assertEqual(calc.subtract(-1, -1), 0)
 
     def test_multiply(self):
-        self.assertEqual(simple_calculator.multiply(2, 3), 6)
-        self.assertEqual(simple_calculator.multiply(-1, 1), -1)
-        self.assertEqual(simple_calculator.multiply(-1, -1), 1)
+        calc = SimpleCalculator()  # Create an instance of SimpleCalculator
+        self.assertEqual(calc.multiply(2, 3), 6)
+        self.assertEqual(calc.multiply(-1, 1), -1)
+        self.assertEqual(calc.multiply(-1, -1), 1)
 
     def test_divide(self):
-        self.assertEqual(simple_calculator.divide(6, 3), 2)
-        self.assertEqual(simple_calculator.divide(-1, 1), -1)
-        self.assertEqual(simple_calculator.divide(-1, -1), 1)
+        calc = SimpleCalculator()  # Create an instance of SimpleCalculator
+        self.assertEqual(calc.divide(6, 3), 2)
+        self.assertEqual(calc.divide(-1, 1), -1)
+        self.assertEqual(calc.divide(-1, -1), 1)
         with self.assertRaises(ValueError):
-            simple_calculator.divide(1, 0)
+            calc.divide(1, 0)
 
 if __name__ == '__main__':
     unittest.main()
